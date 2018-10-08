8d615768b7e80009a24411a0526a8305446b20af
vikashyadav730 <43463193+vikashyadav730@users.noreply.github.com>
2018-10-08 20:30:13 +0530

    Update 1.task2.py

---
+++ 1.task2.py
@@ -0,0 +1,11 @@
+number1 = 63
+number2 = 52
+number3 = 32
+if number1 > number2:
+    print ("greatest is,number1")
+elif number2 > number1:
+    print ("greatest is ,number2")
+elif number3 > number2:
+    print ("greatest is ,number3")
+else:
+    print ("all are equal")
